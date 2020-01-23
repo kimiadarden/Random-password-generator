65b07d8207b2bbb4752183c4d7531a4f6dd36b3a
Kimia Darden <kimiadarden@gmail.com>
2020-01-23 10:43:09 -0500

    creating arrays for all the parameters

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-//  userInput of the password:
-// THEN I choose a userInput of at least 8 characters and no more than 128 characters
 
-// character types:
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
 
-// WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
@@ -18,47 +11,70 @@ var generateBtn = document.querySelector("#generate");
 
 
 // the user input is a string so I added Number function
-
 var userInput = Number(prompt("How many characters would you like your password to contain?"));
 
-if  (userInput>=8 && userInput<=128){
-  confirm("Click Ok to confirm including special characters");
-}
-else if (userInput<=7) {
-  alert("Password length must be at least 8 characters");
-}
+if (userInput >= 8 && userInput <= 128) {
+  var specialChar = confirm("Click Ok to confirm including special characters");
+  if (specialChar == true) {
+    charSet = ["!","@","#","$","%","^","&","*","(",")","_","+","~","`","|","}","{","[","]"]
+  }
+  
+  var numChar = confirm("Click Ok to confirm including numeric characters")
+  if (numChar == true) {
+    numSet = ["0","1","2","3","4","5","6","7","8","9"]
+  }
+  
+  
+  var loChar = confirm("Click Ok to confirm including lowercase characters")
+  if (loChar == true) {
+    lowSet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  }
+  
+  
+  var upCharconfirm = ("Click Ok to confirm including uppercase characters")
+  if (upCharconfirm == true) {
+    upSet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  }
+  
+ 
 
-else {
-  alert("Password length must be less than 129 characters")
-}
-
-
-
-// confirm("Click Ok to confirm including special characters")
-
-
-
-
-// confirm("Click Ok to confirm including numeric characters")
-
-// confirm("Click Ok to confirm including lowercase characters")
-
-// confirm("Click Ok to confirm including uppercase characters")
-
-function generatePassword() {
-
-
-}
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  
+    
+    
+  }
+  else if (userInput <= 7) {
+    alert("Password length must be at least 8 characters");
+  }
+  
+  else {
+    alert("Password length must be less than 129 characters")
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  function generatePassword() {
+    
+    
+  }
+  
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    
+    passwordText.value = password;
+    
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
+  
+  
+  
