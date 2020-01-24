6c170f0e6bd4bf907664eea64c06de84ffc38cef
Kimia Darden <kimiadarden@gmail.com>
2020-01-24 09:17:33 -0500

    password function added

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ var generateBtn = document.querySelector("#generate");
 
 
 // THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
+
+var allChar = [];
 
 
 // the user input is a string so I added Number function
@@ -16,65 +15,76 @@ var userInput = Number(prompt("How many characters would you like your password 
 if (userInput >= 8 && userInput <= 128) {
   var specialChar = confirm("Click Ok to confirm including special characters");
   if (specialChar == true) {
-    charSet = ["!","@","#","$","%","^","&","*","(",")","_","+","~","`","|","}","{","[","]"]
+    charSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "~", "`", "|", "}", "{", "[", "]"]
+    allChar.push(charSet)
   }
-  
+
   var numChar = confirm("Click Ok to confirm including numeric characters")
   if (numChar == true) {
-    numSet = ["0","1","2","3","4","5","6","7","8","9"]
+    numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    allChar.push(numSet)
   }
-  
-  
+
+
   var loChar = confirm("Click Ok to confirm including lowercase characters")
   if (loChar == true) {
-    lowSet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    lowSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    allChar.push(lowSet)
   }
-  
-  
-  var upCharconfirm = ("Click Ok to confirm including uppercase characters")
-  if (upCharconfirm == true) {
-    upSet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-  }
-  
- 
 
-  
-    
-    
+
+  var upCharconfirm = confirm("Click Ok to confirm including uppercase characters")
+  if (upCharconfirm == true) {
+    upSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    allChar.push(upSet)
   }
-  else if (userInput <= 7) {
-    alert("Password length must be at least 8 characters");
+
+
+
+  function generatePassword(l) {
+    var finalPass = "";
+
+    //converting the array into a string 
+    var xString = allChar.toString();
+
+    // removing , from variable x
+    var x = xString.replace(/,/g, "");
+
+
+    for (var i = 0; i < l; i++) {
+      finalPass += x.charAt(Math.floor(Math.random() * userInput));
+    }
+    return finalPass;
   }
-  
-  else {
-    alert("Password length must be less than 129 characters")
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  function generatePassword() {
-    
-    
-  }
-  
-  
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    
-    passwordText.value = password;
-    
-  }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
-  
-  
-  
-  
+  alert(generatePassword(userInput));
+
+
+
+
+
+}
+else if (userInput <= 7) {
+  alert("Password length must be at least 8 characters");
+}
+
+else {
+  alert("Password length must be less than 129 characters")
+}
+
+
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
